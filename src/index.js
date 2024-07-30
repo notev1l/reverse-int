0ee729c6e1b081288ca102efe96f6f879239dfe6
Viktor <skyba.viktor2016@gmail.com>
2024-07-30 15:25:08 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let temp = n.toString().replace('-', '');
+    let result = '';
+    for (let i = 1; i <= temp.length; i++) {
+        result += temp[temp.length-i];
+    }
+    return +result;
 }
